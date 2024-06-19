02e485770eaabe68269c8db3d05a45a9fa2e521a
Romuald_Hanriot <56790180+Phyrum78@users.noreply.github.com>
2024-06-19 11:08:15 +0200

    first table

---
+++ src/main/java/com/exo2/Exercice2/entity/Review.java
@@ -0,0 +1,48 @@
+package com.exo2.Exercice2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "review")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Utilisateur user;
+
+    @ManyToOne
+    @JoinColumn(name = "reviewer_id", nullable = false)
+    private Utilisateur reviewer;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        LocalDateTime updatedAt = LocalDateTime.now();
+    }
+
+
+
+}
---
+++ src/main/java/com/exo2/Exercice2/entity/Utilisateur.java
@@ -0,0 +1,39 @@
+package com.exo2.Exercice2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "utilisateur")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "utilisateur_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+    private String city;
+    private String region;
+    private int age;
+    private String interests;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        LocalDateTime updatedAt = LocalDateTime.now();
+    }
+}
