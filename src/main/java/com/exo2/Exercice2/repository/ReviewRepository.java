00c54808f58e03d86d2aa7ffc1b6487502fe77b4
Romuald_Hanriot <56790180+Phyrum78@users.noreply.github.com>
2024-06-21 10:27:33 +0200

    index

--- src/main/java/com/exo2/Exercice2/repository/ReviewRepository.java
+++ src/main/java/com/exo2/Exercice2/repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package com.exo2.Exercice2.repository;
 import com.exo2.Exercice2.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByRating(int rating);
 }
--- src/main/java/com/exo2/Exercice2/repository/UserRepository.java
+++ src/main/java/com/exo2/Exercice2/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.exo2.Exercice2.repository;
 import com.exo2.Exercice2.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<Utilisateur, Long> {
+    List<Utilisateur> findByEmail(String email);
 }
